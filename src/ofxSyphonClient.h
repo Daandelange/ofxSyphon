3bd75a677f46874370a3a944bcddf568ea42258f
daan <idaany@gmail.com>
2024-09-26 20:46:59 +0200

    Add Client.isConnected to query if a server is connected.

--- src/ofxSyphonClient.h
+++ src/ofxSyphonClient.h
@@ -17,6 +17,7 @@ class ofxSyphonClient {
 
     void setup();
     bool isSetup() const;
+    bool isConnected() const;
     
     void set(const ofxSyphonServerDescription &_server);
     void set(const std::string &_serverName, const std::string &_appName);
